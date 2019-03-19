ae23df48e84b2e66655c0e1043401d7554da9a62
nhan98453 <nhan98453@gmail.com>
2019-03-19 11:53:08 +0700

    hello

--- database/migrations/2019_03_18_150458_nhan__tao_bang__customer.php
+++ database/migrations/2019_03_18_150458_nhan__tao_bang__customer.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class NhanTaoBangCustomer extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        //
+        Schema::create('Customer',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('Ten');
+            $table->string('sdt');
+            $table->string('email');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +30,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
+        Schema::drop('Customer');
     }
 }
