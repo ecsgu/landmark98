00decd980f11ab4ad0e8cb3413eddbd949287bb9
nhan98453 <nhan98453@gmail.com>
2019-03-21 01:13:22 +0700

    insert topic

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,47 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	Model::unguard();
-        // $this->call(UsersTableSeeder::class);
         $this->call(CustomerSeeder::class);
-    }
-}
-
-class CustomerSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-        $faker = Faker\Factory::create();
-        $limits=10;
-
-        for($i=0;$i<$limits;$i++)
-        {
-        	$username = $faker->username;
-        	$email = $faker->unique()->safeEmail;
-	        DB::table('customer')->insert([
-	           	'id' => $username,
-	           	'name' => $faker->name,
-	            'gender' => rand(1,2)%2?'Nam':'Nữ', 
-	            'email' => $email,
-	            'phone_number' => rand(1111111111,9999999999),
-	            'room' => str_random(5),
-	            'created_at' => now(),
-	            'updated_at' => now()
-	        ]);
-	        DB::table('account')->insert([
-	           	'username' => $username,
-	           	'password' => hash('sha256', str_random(9)),
-	           	'email' => $email,
-	           	'role' => '1',
-	           	'created_at' => now(),
-	            'updated_at' => now()
-	        ]);
-    	}
+        $this->call(TopicSeeder::class);
     }
 }
---
+++ database/seeds/TopicSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class TopicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $account = DB::table('account')->get();
+        $faker = Faker\Factory::create();
+        $limits=10;
+
+        for($i=0;$i<$limits;$i++)
+        {
+            DB::table('topic')->insert([
+                'caption' => $faker->sentence,
+                'image' => 'image/'.$faker->word,
+                'username' => $account->random()->Username, 
+                'status' => rand(1,3),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
