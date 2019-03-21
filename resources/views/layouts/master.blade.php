e09f27087df7e96dd84cae7b7c40ba6a60f48ced
Trương Vĩ Huy <truongvihuy@gmail.com>
2019-03-21 22:08:20 +0700

    Bar oke

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Landmark 98</title>
         <link href="{{ asset('css/VH.css') }}" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="{{ asset('js/VH.js') }}" type="text/javascript"></script>
         <link href="{{ asset('logo.ico') }}" rel="shortcut icon">
         <style>
@@ -17,22 +18,33 @@
     </head>
     <body id="main"> 
     <!-- Header -->
-    <div class="vh-container vh-safety-blue vh-bar">
-        <div class="vh-bar-item"><a href="{{ asset('') }}"><img src="{{ asset('logo.ico') }}" width="50px"></a></div>
-        <label class="vh-switch-round vh-right vh-margin">
+    <div class="vh-container vh-bar vh-top vh-safety-blue vh-faster" id="navbar" style="z-index:4">
+        <div class="vh-bar-item"><a href="{{ asset('') }}"><img src="{{ asset('logo.ico') }}" width="40px"></a></div>
+        <div class="vh-bar-item vh-right" style="margin:8px 16px;"><span class="glyphicon glyphicon-triangle-bottom"></span></div>
+        <label class="vh-switch-round vh-right" style="margin:8px 16px;">
             <input type="checkbox">
             <span class="vh-switch-slider" onclick="checknightmode();"></span>
         </label>
     </div>
-    
-		<!-- Container -->
+    <!-- Container -->
+    <div class="vh-container" style="margin-top:70px;">
     @yield('Container')
-		<!-- Footter -->
-    </body>
+    </div>
     <script>
+      var prevScrollpos = window.pageYOffset;
+      window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+          document.getElementById("navbar").style.top = "0";
+        } else {
+          document.getElementById("navbar").style.top = "-68px";
+        }
+        prevScrollpos = currentScrollPos;
+      }
       function checknightmode(){
-          var nig = document.getElementById('main');
-          nig.classList.toggle('night-mode');
+        var nig = document.getElementById('main');
+        nig.classList.toggle('night-mode');
       }
     </script>
+    </body>
 </html>
