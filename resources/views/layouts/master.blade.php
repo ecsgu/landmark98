8ff5174c887a7d3a97e3e2f6e5ef269ae3eb8852
Trương Vĩ Huy <truongvihuy@gmail.com>
2019-03-21 13:53:40 +0700

    Đã có chế độ night mode

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,25 +5,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Landmark 98</title>
         <link href="{{ asset('css/VH.css') }}" rel="stylesheet" type="text/css"/>
-        <link href="{{ asset('css/Animation.css') }}" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <script src="{{ asset('js/VH.js') }}" type="text/javascript"></script>
         <link href="{{ asset('logo.ico') }}" rel="shortcut icon">
         <style>
-          body{
+          .night-mode{
             background-color:black;
             color:white;
           }
         </style>
     </head>
-    <body> 
+    <body id="main"> 
     <!-- Header -->
-    <div class="vh-container vh-safety-blue">
-        <div class="vh-margin" href="{{ asset('') }}"><img src="{{ asset('logo.ico') }}" width="50px"></div>
-          
+    <div class="vh-container vh-safety-blue vh-bar">
+        <div class="vh-bar-item"><a href="{{ asset('') }}"><img src="{{ asset('logo.ico') }}" width="50px"></a></div>
+        <label class="vh-switch-round vh-right vh-margin">
+            <input type="checkbox">
+            <span class="vh-switch-slider" onclick="checknightmode();"></span>
+        </label>
     </div>
+    
 		<!-- Container -->
     @yield('Container')
 		<!-- Footter -->
     </body>
+    <script>
+      function checknightmode(){
+          var nig = document.getElementById('main');
+          nig.classList.toggle('night-mode');
+      }
+    </script>
 </html>
