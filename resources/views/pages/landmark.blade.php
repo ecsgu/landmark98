8699a13c4b9ae5e0be7a23d536326a630bcd568a
nhan98453 <nhan98453@gmail.com>
2019-03-26 18:31:29 +0700

    fix

--- resources/views/pages/landmark.blade.php
+++ resources/views/pages/landmark.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.master')
 
 @section('Container')
@@ -10,15 +11,6 @@
         </div>
         @foreach($Topic as $topic)
         <div class="vh-col l8 m12 s12">
-            <!-- Phần đăng tin -->
-            <div class="vh-card-4 vh-round vh-padding vh-margin-top">
-                <!-- User post -->
-                <div class="vh-row">
-                    <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"> </div>
-                    <div class="vh-col l10 m10 s10"> 
-                    <textarea style="resize:none"></textarea></div>
-                </div>
-            </div>
             <!-- Bài post -->
             <div class="vh-card-4 vh-round vh-padding vh-margin-top">
                 <!-- User post -->
@@ -40,60 +32,10 @@
                     <div class="vh-row vh-margin-top">
                         <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"></div>
                         <div class="vh-col l11 m11 s11">
-                            <input class="vh-input" type="text" placeholder="Viết bình luận" />
+                            <input class="vh-input" type="text" />
                         </div>
                     </div>
                     @foreach($topic->comment as $comment)
-                    <div class="vh-row vh-margin-top">
-<<<<<<< HEAD
-                        <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"></div>
-                        <div class="vh-col l11 m11 s11">
-                            <a href="#">Trí Nhân</a> Các bạn comment tương tác coi, Các bạn comment tương tác coi, Các bạn comment tương tác coi,Các bạn comment tương tác coi
-                            <div class="vh-small vh-text-gray">24 tháng 3 năm 2019, lúc 7:10</div>
-                        </div>
-                    </div>
-                    <div class="vh-row vh-margin-top">
-                        <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"></div>
-                        <div class="vh-col l11 m11 s11">
-                            <a href="#">Trí Nhân</a> Huhu mấy bạn không ai cmt hết
-                            <div class="vh-small vh-text-gray">24 tháng 3 năm 2019, lúc 7:10</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="#">Xem thêm</a>
-            </div>
-            <!-- Bài post -->
-            <div class="vh-card-4 vh-round vh-padding vh-margin-top">
-                <!-- User post -->
-                <div class="vh-row">
-                    <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"> </div>
-                    <div class="vh-col l11 m11 s11">
-                        <a href="#">Trí Nhân</a>
-                        <div class="vh-small vh-text-gray">23 tháng 3 năm 2019, lúc 15:10</div>
-                    </div>
-                </div>
-                <!-- Caption -->
-                <div class="vh-margin-top">
-                    <p>Ngày cuối tuần nhẹ nhàng sương sương</p>
-                    <p>Chạy chương trình từ hôm thứ bảy đến giờ</p>
-                </div>
-                <!-- Image -->
-                <img class="vh-image" src="{{ asset('upload\DSC01144.jpg') }}" />
-                <!-- Comments -->
-                <div class="vh-padding">
-                    <div class="vh-row vh-margin-top">
-                        <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"></div>
-                        <div class="vh-col l11 m11 s11">
-                            <input class="vh-input" type="text" placeholder="Viết bình luận" />
-                        </div>
-                    </div>
-                    <div class="vh-row vh-margin-top">
-                        <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ asset('upload\a40aeff2d729cd25808ac2d41721e32e.jpg') }}" width="40px"></div>
-                        <div class="vh-col l11 m11 s11">
-                            <a href="#">Trí Nhân</a> Các bạn comment tương tác coi, Các bạn comment tương tác coi, Các bạn comment tương tác coi,Các bạn comment tương tác coi
-                            <div class="vh-small vh-text-gray">24 tháng 3 năm 2019, lúc 7:10</div>
-                        </div>
-                    </div>
                     <div class="vh-row vh-margin-top">
                         <div class="vh-col l1 m1 s1"><img class="vh-circle" src="{{ $comment->customer->image }}" width="40px"></div>
                         <div class="vh-col l11 m11 s11">
