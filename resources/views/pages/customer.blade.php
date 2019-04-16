f9424a08db04ff2234afc78160509c5b5d6da0d7
nhan98453 <nhan98453@gmail.com>
2019-04-16 11:45:06 +0700

    test

--- resources/views/pages/customer.blade.php
+++ resources/views/pages/customer.blade.php
@@ -43,7 +43,7 @@
                     <table class="vh-table-all">
                         <tr>
                             <th>Tên đăng nhập:</th>
-                            <td>tvhuy</td>
+                            <td>{{ $Customer->id }}</td>
                         </tr>
                         <tr>
                             <th>Mật khẩu khẩu:</th>
@@ -81,7 +81,7 @@
             <div class="vh-card-4 vh-round vh-padding vh-margin-top">
                 <!-- User post -->
                 <div class="vh-row">
-                    <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{ $topic->customer->image }}" width="40px"> </div>
+                    <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{ asset($topic->customer->image) }}" width="40px"> </div>
                     <div class="vh-col l11 m10 s10">
                         <a href="#">{{ $topic->customer->name }}</a>
                         <div class="vh-small vh-text-gray">{{ $topic->created_at }}</div>
@@ -93,19 +93,19 @@
                 </div>
                 <!-- Image -->
                 @if(isset($topic->image))
-                <img src="{{ $topic->image }}" width="100%" />
+                <img src="{{ asset($topic->image) }}" width="100%" />
                 @endif
                 <!-- Comments -->
                 <div class="vh-padding">
                     <div class="vh-row vh-margin-top">
-                        <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{Session::get('account')->customer->image}}" width="40px"></div>
+                        <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{asset(Session::get('account')->customer->image)}}" width="40px"></div>
                         <div class="vh-col l11 m10 s10">
                             <textarea onfocus="this.attributes['rows'].value = 3" onblur="this.attributes['rows'].value = 1" class="vh-border-0" placeholder="Bạn hãy nhập bình luận..." style="width:100%" rows=1></textarea>
                         </div>
                     </div>
                     @foreach($topic->comment as $comment)
                     <div class="vh-row vh-margin-top">
-                        <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{ $comment->customer->image }}" width="40px"></div>
+                        <div class="vh-col l1 m2 s2"><img class="vh-circle" src="{{ asset($comment->customer->image) }}" width="40px"></div>
                         <div class="vh-col l11 m10 s10">
                             <a href="#">{{ $comment->customer->name }}</a> 
                             {{ $comment->caption }}
