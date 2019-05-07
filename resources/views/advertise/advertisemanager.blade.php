d29e7f8b11cf31bd1778cacec0535ae9cdb27440
ngochau011298 <48237541+ngochau011298@users.noreply.github.com>
2019-05-07 23:40:56 +0700

    xong giao dien quan li advertise

--- resources/views/advertise/advertisemanager.blade.php
+++ resources/views/advertise/advertisemanager.blade.php
@@ -1,5 +1,57 @@
 @extends('layouts.advertise')
 
 @section('Container')
-	
+	<div class="breadcrumbs">
+        <div class="col-sm-4">
+            <div class="page-header float-left">
+                <div class="page-title">
+                    <h1>Các quảng cáo của tôi</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-11 ml-5 mt-3">
+    	<table style="background: #fff" class="table table-hover">
+			<thead>
+			<tr>
+			  <th scope="col">STT</th>
+			  <th scope="col">Link quảng cáo</th>
+			  <th scope="col">Ngày bắt đầu</th>
+			  <th scope="col">Ngày kết thúc</th>
+			  <th scope="col">Vị trí quảng cáo</th>
+			  <th scope="col">Thành tiền</th>
+			  <th scope="col">Trạng thái</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+			  <th scope="row">1</th>
+			  <td>Mark</td>
+			  <td>Otto</td>
+			  <td>@mdo</td>
+			  <td>@mdo</td>
+			  <td>@mdo</td>
+			  <td>@mdo</td>
+			</tr>
+			<tr>
+			  <th scope="row">2</th>
+			  <td>Jacob</td>
+			  <td>Thornton</td>
+			  <td>@fat</td>
+			  <td>@fat</td>
+			  <td>@fat</td>
+			  <td>@fat</td>
+			</tr>
+			<tr>
+			  <th scope="row">3</th>
+			  <td>Larry</td>
+			  <td>the Bird</td>
+			  <td>@twitter</td>
+			  <td>@twitter</td>
+			  <td>@twitter</td>
+			  <td>@twitter</td>
+			</tr>
+			</tbody>
+		</table>
+    </div>
 @endsection
